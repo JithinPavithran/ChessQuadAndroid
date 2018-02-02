f4d3badcdb164f30c81f51ef136f4ac4eff3d684
JithinPavithran <jithinpavithran96@gmail.com>
2018-02-02 16:15:06 +0530

    Added Google login skeleton

--- app/src/main/java/com/jithin/games/chessquad/Home.java
+++ app/src/main/java/com/jithin/games/chessquad/Home.java
@@ -1,12 +1,23 @@
 package com.jithin.games.chessquad;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class Home extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+    static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,15 +25,67 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-    public void login(View view){
-        Log.d("Home", "Logging in");
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
     }
 
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.d("Home", "Sign in Success");
+            Log.d("UserName", account.getDisplayName());
+        } catch (ApiException e) {
+            Log.w("Home", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+
+    public void login(View view){
+        Log.d("Home", "Logging in");
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            Log.d("Home", "Login Success");
+            Log.d("Already signed in User", account.getDisplayName());
+            return;
+        }
+        Log.d("Home", "Trying Login");
+        signIn();
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+
+
+    public void logout(View view) {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("Home", "Logout Complete");
+                    }
+                });
+    }
 
     public void goToGame(View view){
         Log.d("Home", "Moving to Game activity");
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
-
 }
