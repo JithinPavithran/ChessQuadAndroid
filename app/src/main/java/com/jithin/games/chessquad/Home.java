f706b9902bb8822322a5eb7622a2372e9f0f7bdd
JithinPavithran <jithinpavithran96@gmail.com>
2018-05-26 19:33:33 +0530

    Google sign in completed

--- app/src/main/java/com/jithin/games/chessquad/Home.java
+++ app/src/main/java/com/jithin/games/chessquad/Home.java
@@ -1,15 +1,14 @@
 package com.jithin.games.chessquad;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.*;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -33,45 +32,57 @@ public class Home extends AppCompatActivity {
 
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                // Signed in successfully, show authenticated UI.
+                if(account!=null){
+                    Log.d("Home", "Manual login Success");
+                    Log.d("Home", "Email-id: "+account.getEmail());
+
+                }
+                else{
+                    Log.d("Home", "Even Manual login failed");
+                }
+            } catch (ApiException e) {
+                Log.d("Home", "Exception in manual login");
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w("Home", "signInResult:failed code=" + e.getStatusCode());
+            }
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            Log.d("Home", "Sign in Success");
-            Log.d("UserName", account.getDisplayName());
-        } catch (ApiException e) {
-            Log.w("Home", "signInResult:failed code=" + e.getStatusCode());
-        }
-    }
 
 
     public void login(View view){
-        Log.d("Home", "Logging in");
+        Log.d("Home", "Trying to Log in");
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestEmail()
-        .build();
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null){
-            Log.d("Home", "Login Success");
-            Log.d("Already signed in User", account.getDisplayName());
-            return;
+        if(account!=null) {
+            Log.d("Home", "User already logged in");
         }
-        Log.d("Home", "Trying Login");
-        signIn();
+        else{
+            Log.d("Home", "User not logged in");
+            Log.d("Home", "Trying manual login");
+
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        }
+
     }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-
-
-
 
     public void logout(View view) {
         mGoogleSignInClient.signOut()
